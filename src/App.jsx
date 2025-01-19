b021a59622e419e557c7b435a3d430a311e6d940
PrasadRavindra2000 <wmprasadwijesinghe@gmail.com>
2025-01-19 19:27:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+const App = () => {
+
+  let heroData = [
+    {text1:"Dive into", text2:"What you love"},
+    {text1:"Indulge" , text2:"your passions"},
+    {text1:"Give in to" , text2:"your passions"},
+  ]
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
